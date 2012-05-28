c16baf91204c4e5701220cebaa3e135625e77d40
gfxmonk <tim@gfxmonk.net>
2012-05-28 22:08:58 +1000

    2.7 restriction is a little harsh, it seems to work fine on 2.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
 	entry_points={'console_scripts': ['piep=piep.main:main']},
 	name='piep',
 	url='http://gfxmonk.net/dist/0install/piep.xml',
-	install_requires=['setuptools', 'python<3', 'pygments'],
+	install_requires=['setuptools', 'python>=2.6<3', 'pygments'],
 	version='0.6.2',
 	long_description='\n**Note**: This package has been built automatically by\n`zero2pypi <http://gfxmonk.net/dist/0install/zero2pypi.xml>`_.\nIf possible, you should use the zero-install feed instead:\nhttp://gfxmonk.net/dist/0install/piep.xml\n\n----------------\n\npiep\n====\n\nBringing the power of python to stream editing\n----------------------------------------------\n\n| Zero install feed:\n| http://gfxmonk.net/dist/0install/piep.xml\n|\n| Online documentation:\n| http://gfxmonk.net/dist/doc/piep/\n|\n| Source Code / Issues:\n| https://github.com/gfxmonk/piep/\n|\n| Cheese shop entry:\n| http://pypi.python.org/pypi/piep\n',
 	description="unix-style stream manipulation with python's syntax",
