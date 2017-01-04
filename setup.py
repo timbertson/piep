a072734fd044e39478669e728f43e927a8ff2111
Tim Cuthbertson <tim@gfxmonk.net>
2017-01-04 22:27:24 +1100

    0.8.1

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
 	name='piep',
 	url='http://gfxmonk.net/dist/0install/piep.xml',
 	install_requires=['python>=2.6', 'pygments'],
-	version='0.8.0',
+	version='0.8.1',
 	description="unix-style stream manipulation with python's syntax",
 )
