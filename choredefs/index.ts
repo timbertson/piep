9c032f593bbcb977b3b6048081bfa482dfe9a148
Tim Cuthbertson <tim@gfxmonk.net>
2022-09-04 20:24:37 +1000

    chored

---
+++ choredefs/index.ts
@@ -0,0 +1,1 @@
+export * from 'https://raw.githubusercontent.com/timbertson/chored-timbertson/184facdadcc6ee655ded41c14a079b33c722f0a4/lib/pypi.ts'
---
+++ choredefs/render.ts
@@ -0,0 +1,7 @@
+import * as Render from 'https://deno.land/x/chored@v0.2.3/lib/render.ts'
+
+export default async function(opts: {}) {
+  Render.render([
+    // TODO add your own generated files
+  ])
+}
